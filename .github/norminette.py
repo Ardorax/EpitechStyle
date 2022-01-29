cdc6c6c3acda9f6596390660ef3b903208784526
Ardorax <valentin03dury@gmail.com>
2022-01-29 17:56:56 +0100

    file

--- .github/norminette.py
+++ .github/norminette.py
@@ -1,5 +1,8 @@
 import imp
 import os
+from xml.dom.minidom import Element
+
+from pkg_resources import WorkingSet
 
 class Norminette:
     def __init__(self) -> None:
@@ -10,7 +13,9 @@ class Norminette:
         self.current_file = ""
         self.file_trace = ""
         self.count_line = -1
+        self.wrong_file = []
         self.folder_explorer(".")
+        self.add_wrong_files()
 
     def folder_explorer(self, path) -> None:
         for element in os.listdir(path):
@@ -24,10 +29,9 @@ class Norminette:
                     self.file_explorer(path + "/" + element)
                 else:
                     self.current_file = path + "/" + element
-                    self.error_creator("Wrong file", 0)
+                    self.wrong_file.append(path + "/" + element)
 
     def file_explorer(self, path):
-        self.file_trace = ""
         self.current_file = path
         file = open(path)
         self.count_line = 1
@@ -38,8 +42,9 @@ class Norminette:
         # End of file
         self.count_line = -1
         if (self.file_trace != ""):
-            self.trace += "\n# In file " + path + "\n"
-            self.trace += self.file_trace
+            self.trace += "# In file " + path + "\n"
+            self.trace += self.file_trace + "\n"
+            self.file_trace = ""
 
     def error_creator(self, desc: str, type : int):
         error = ["[MAJOR]", "[MINOR]", "[INFO]"]
@@ -55,5 +60,10 @@ class Norminette:
         else:
             self.file_trace += f"{error[type]}: At line {self.count_line} : {desc}\n"
 
+    def add_wrong_files(self):
+        if (len(self.wrong_file) != 0):
+            self.trace += "# Wrong files :\n"
+            for file in self.wrong_file:
+                self.trace += f"{file}\n"
     # Here import coding style function
     from ardorax import trailing_spaces
